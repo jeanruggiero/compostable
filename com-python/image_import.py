c0be13dc19a272905349e9890f657c84dde3f20b
Jean Ruggiero <jeanruggiero@gmail.com>
2020-02-08 12:33:45 -0500

    Recognize spoon outline

--- com-python/image_import.py
+++ com-python/image_import.py
@@ -1,10 +1,22 @@
-from skimage import io
+from skimage import io, filters
 import os
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+from skimage.color import rgb2gray
 
 image = io.imread('../images/plastic_dark_background.jpg')
+grayscale = rgb2gray(image)
 
 print(image.shape)
+print(dir(image))
 
+edges = filters.sobel(grayscale)
+io.imshow(edges)
+io.show()
+
+io.imshow(image)
+io.show()
 
 
