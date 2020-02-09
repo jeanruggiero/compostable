0a6988a9dd898a5bb5be2f61ad87dd7ac11baff7
Jean Ruggiero <jeanruggiero@gmail.com>
2020-02-08 19:27:53 -0500

    Added notebook to find average value in lightest area of the image

--- com-python/image_import.py
+++ com-python/image_import.py
@@ -6,7 +6,12 @@ import matplotlib.pyplot as plt
 
 from skimage.color import rgb2gray
 
+from mpl_toolkits import mplot3d
+
+
 image = io.imread('../images/plastic_dark_background.jpg')
+image = io.imread('../images/sp5.png')
+
 grayscale = rgb2gray(image)
 
 print(image.shape)
@@ -18,5 +23,12 @@ io.show()
 
 io.imshow(image)
 io.show()
+
+print(edges)
+
+fig = plt.figure();
+ax = plt.axes(projection='3d')
+ax.plot_surface(range(image.shape[0]), range(image.shape[1]), image[1])
+plt.show()
 
 
