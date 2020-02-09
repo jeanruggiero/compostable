f8e493922b0baad58343e6246ecd83a0a16e631e
Jean Ruggiero <jeanruggiero@gmail.com>
2020-02-08 23:07:24 -0500

    Fixed issue with find_lightest_area_value

---
+++ com-react/src/components/Theme.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { createMuiTheme } from "@material-ui/core/styles"
+import green from '@material-ui/core/colors/green'
+
+const Theme = createMuiTheme({palette: {primary: green}});
+
+export default Theme;
